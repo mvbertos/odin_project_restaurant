545e0e0120d73353bb0e04a8accbaa33d08d7094
mvbertos <matheus.bertos2@gmail.com>
2026-04-15 14:17:05 -0300

    added robot font as font family for the page

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 import path from "node:path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import test from "node:test";
 
 export default {
   mode: "development",
@@ -27,6 +28,10 @@ export default {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
       },
     ],
